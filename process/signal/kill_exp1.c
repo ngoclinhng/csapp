6671c9db016120f24daa93e611ae50115f1f0957
Linh Ng <mail2ngoclinh@gmail.com>
2017-11-22 17:43:40 +0700

    Implement kill_exp1

---
+++ process/signal/kill_exp1.c
@@ -0,0 +1,22 @@
+#include "process.h"
+
+int main(void)
+{
+    pid_t pid;
+    
+    /* Child sleeps until SIGKILL signal recieved, then dies */
+   if ((pid = Fork()) == 0) {
+        printf("I am about to sleep until SIGKILL signal arrived\n");
+        Pause();
+        printf("Control should never reach here\n");
+        exit(0);
+   }
+   
+   /* Parent sleeps for 2 seconds */
+   Sleep(2);
+
+   /* Parent sends a SIGKILL signal to a child */
+   Kill(pid, SIGKILL);
+
+   return 0;
+}
